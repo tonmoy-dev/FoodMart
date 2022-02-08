94e901e257d074d697febc4bff9334f3efa6d6ff
Tonmoy Roy <rxdevs100@gmail.com>
2022-02-08 13:52:04 +0600

    navbar first commit

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 const Navbar = () => {
     return (
         <div>
-            <h1>Navbar</h1>
+            <h1 className="text-center text-3xl underline">Welcome to FoodMart!</h1>
         </div>
     );
 };
