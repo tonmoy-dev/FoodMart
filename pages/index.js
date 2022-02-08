5a85067d035632055468f71e65864f8ee808d6e4
Tonmoy Roy <rxdevs100@gmail.com>
2022-02-08 12:24:20 +0600

    FoodMart project setup

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js - FoodMart Team!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>src/Components/Home</code> <br />
+          and create your own Component folder in Home folder.
         </p>
 
         <div className={styles.grid}>
