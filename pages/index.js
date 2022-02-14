658aea9449a333b1c3b12874873aac56c4d53bba
Avishekdevnath <avishekdevnath@gmail.com>
2022-02-14 18:18:10 +0600

    A

--- pages/index.js
+++ pages/index.js
@@ -31,13 +31,11 @@ export default function Home({products}) {
 }
 
 export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
+
   const res = await fetch('https://foodmart11.herokuapp.com/products')
   const products = await res.json()
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
+
   return {
     props: {
       products,
