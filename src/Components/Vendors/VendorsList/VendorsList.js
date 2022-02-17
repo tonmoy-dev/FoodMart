1e7f40a05d1e38b9683d77b2c074071c686031f5
Avishekdevnath <avishekdevnath@gmail.com>
2022-02-18 00:59:43 +0600

    A

--- src/Components/Vendors/VendorsList/VendorsList.js
+++ src/Components/Vendors/VendorsList/VendorsList.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 
 const VendorsList = () => {
-
+    // hello
     let Allvendors = require("../../../assets/Api/Vendors.json");
     console.log(Allvendors);
     return (
