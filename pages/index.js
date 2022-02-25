97d133e2fc654523be65c330c879e4427f3b9c07
Anik-Nath <anikdevnath03112000@gmail.com>
2022-02-25 14:44:30 +0600

    error solve

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-// import AddProduct from '../src/Components/Dashboard/Admin/AddProduct/AddProduct';
-// import Checkout from '../src/Components/Dashboard/User/Checkout/Checkout/Checkout';
 import Banner from "../src/Components/Home/Banner/Banner";
 import BestSells from "../src/Components/Home/BestSells/BestSells";
-// import Blogs from '../src/Components/Home/Blogs/Blogs';
+import Blogs from '../src/Components/Home/Blogs/Blogs';
 import Brands from "../src/Components/Home/Brands/Brands";
 import Categories from "../src/Components/Home/Categories/Categories";
 import DealsofDay from "../src/Components/Home/DealsofDay/DealsofDay";
@@ -13,22 +11,11 @@ import NewsLetter from "../src/Components/Home/NewsLetter/NewsLetter";
 import PopularProducts from "../src/Components/Home/PopularProducts/PopularProducts";
 import ProductOffer from "../src/Components/Home/ProductOffer/ProductOffer";
 import Reviews from "../src/Components/Home/Reviews/Reviews";
-import AddBlog from "../pages/dashboard/user/add-blog"
-import Blogs from "../pages/blogs/all-blogs"
-// import AllProducts from '../src/Components/Products/AllProducts/AllProducts';
-// import Vendors from '../src/Components/Vendors/Vendors/Vendors';
-// import VendorsList from '../src/Components/Vendors/VendorsList/VendorsList';
-// import AllProducts from "./products/all-products";
-
-import ContactUs from "./contents/contact-us";
-
-
 
 export default function Home({ reviews, products, blogs }) {
-
   return (
     <div className=" ">
-      {/* <Navbar></Navbar>
+      <Navbar></Navbar>
       <Banner></Banner>
       <PopularProducts products={products}></PopularProducts>
       <DealsofDay></DealsofDay>
@@ -36,44 +23,24 @@ export default function Home({ reviews, products, blogs }) {
       <Categories></Categories>
       <ProductOffer></ProductOffer>
       <DeliverySteps></DeliverySteps>
-      <Reviews reviews={reviews}></Reviews> */}
-      {/* <Blogs blogs={blogs}></Blogs> */}
-      {/* <Brands></Brands>
+      <Reviews reviews={reviews}></Reviews>
+      <Blogs blogs={blogs}></Blogs>
+      <Brands></Brands>
       <NewsLetter></NewsLetter>
       <Footer></Footer>
-      {/* <SingleBlog comments={comments}></SingleBlog> */}
-
-      {/* <AllProducts products={products}></AllProducts> */}
-      {/* <Checkout products={products}></Checkout> */}
-      {/* <Vendors></Vendors> */}
-      {/* <VendorsList></VendorsList> */}
-      {/* <AddProduct></AddProduct> */}
-
-
-      {/* <add-blog></add-blog> */}
-
-      <AddBlog></AddBlog>
-
-      {/* <ContactUs></ContactUs> */}
     </div>
   );
 }
 
 export async function getStaticProps() {
+  const reviews_res = await fetch("http://foodmart11.herokuapp.com/reviews");
+  const reviews = await reviews_res.json();
 
-    const reviews_res = await fetch("http://foodmart11.herokuapp.com/reviews");
-    const reviews = await reviews_res.json();
+  const products_res = await fetch("http://foodmart11.herokuapp.com/products");
+  const products = await products_res.json();
 
-    const products_res = await fetch("http://localhost:5000/products");
-    const products = await products_res.json();
-  
-    const blogs_res = await fetch("http://foodmart11.herokuapp.com/blogs");
-    const blogs = await blogs_res.json();
-  
-  // // const blogs_res = await fetch("http://foodmart11.herokuapp.com/blogs");
-  // const blogs_res = await fetch("http://localhost:5000/blogs");
-  // const blogs = await blogs_res.json();
-
+  const blogs_res = await fetch("http://foodmart11.herokuapp.com/blogs");
+  const blogs = await blogs_res.json();
 
   return {
     props: { reviews, products, blogs },
