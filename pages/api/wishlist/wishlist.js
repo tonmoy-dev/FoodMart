435b6b12470c54d55c883e9a69bbe61483b79563
anamikasharma15 <anamikaanuctg@gmail.com>
2022-03-02 00:14:43 +0600

    wishlist

---
+++ pages/api/wishlist/wishlist.js
@@ -0,0 +1,29 @@
+import clientPromise from "../../../lib/mongodb";
+
+export default async function wishlistHandler(req, res) {
+    const client = await clientPromise;
+    const db = client.db("foodmart_shop");
+
+    const { method } = req;
+
+    // load all 
+    if (method === "GET") {
+        const wishlist = await db.collection("wishlist").find({}).toArray();
+        res.json(wishlist);
+    }
+
+    if (method === "POST") {
+        const wishlistData = req.body;
+        const addwishlist = await db.collection("wishlist").insertOne(wishlistData);
+        res.json(addwishlist);
+
+        /* console.log(req.body); 
+        console.log(req.query) 
+        console.log(req.method); // POST
+        console.log(req.headers.host); // localhost:3000
+        console.log(req.url); // /api/...
+        res.status(200).json({ message: "success" }); */
+      }
+    
+}
+
