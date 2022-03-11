92b4f1e1da58d9d6816a96397f6e09172cb1306a
Anik-Nath <anikdevnath03112000@gmail.com>
2022-03-11 13:18:50 +0600

    change products

--- src/Components/Products/Products/Products.js
+++ src/Components/Products/Products/Products.js
@@ -19,7 +19,7 @@ const Products = ({ products }) => {
       product_price: product_price,
       user_rating: user_rating,
       product_stock: product_stock,
-      product_imageUrl: product_imageUrl.thumbnail,
+      product_imageUrl: product_imageUrl,
 
      }).then((response) => {
 
@@ -42,7 +42,7 @@ const Products = ({ products }) => {
       product_price: product_price,
       user_rating: user_rating,
       product_stock: product_stock,
-      product_imageUrl: product_imageUrl.thumbnail,
+      product_imageUrl: product_imageUrl,
       produc_Details: produc_Details
 
     }).then((response) => {
@@ -159,7 +159,7 @@ const Products = ({ products }) => {
                   {" "}
                   <Image
                     className="p-8 rounded-t-lg"
-                    src={product_imageUrl.thumbnail}
+                    src={product_imageUrl}
                     alt="product image"
                     height="180"
                     width="200"
@@ -282,7 +282,7 @@ const Products = ({ products }) => {
                       onClick={() =>
                         addToCartHandler(
                           product_title,
-                          product_imageUrl.thumbnail,
+                          product_imageUrl,
                           product_price,
                           produc_Details
                         )
