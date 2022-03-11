518487b92568b3fecab70d2665cc19da0661fa82
Anik-Nath <anikdevnath03112000@gmail.com>
2022-03-11 14:21:38 +0600

    faq

--- pages/contents/faq.js
+++ pages/contents/faq.js
@@ -5,10 +5,9 @@ import {
   AccordionItem,
   AccordionItemButton,
   AccordionItemHeading,
-  AccordionItemPanel
+  AccordionItemPanel,
 } from "react-accessible-accordion";
 import "react-accessible-accordion/dist/fancy-example.css";
-
 
 const Faq = () => {
   return (
@@ -27,7 +26,7 @@ const Faq = () => {
               <div className="px-2 w-2/4">
                 <Image
                   className=""
-                  src="https://i.ibb.co/KjQW4kx/3567801.jpg"
+                  src="https://i.ibb.co/CPMfDrx/FAQs.gif"
                   alt="Picture of the author"
                   width={500}
                   height={500}
@@ -35,23 +34,28 @@ const Faq = () => {
                 />
               </div>
               <div className="px-2 w-2/4">
-              <Accordion allowZeroExpanded>
-                  <AccordionItem>
+                <Accordion preExpanded={["a"]} allowZeroExpanded>
+                  <AccordionItem uuid="a">
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                      <span className="text-3xl font-semibold text-green-500">What is FoodMart Services?</span>
+                        <span className="text-xl font-semibold text-green-500">
+                          What is FoodMart Services?
+                        </span>
                       </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                       <div className="flex mt-8">
-                    <span className="border border-green-500"></span>
+                        <span className="border border-green-500"></span>
 
-                    <p className="max-w-3xl  text-gray-500 dark:text-gray-400 bg-green-100 p-10">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Magni, eum quae. Harum officiis reprehenderit ex quia
-                      ducimus minima id provident molestias optio nam vel..
-                    </p>
-                  </div>
+                        <p className="max-w-3xl  text-gray-500 dark:text-gray-400 bg-green-100 p-10">
+                          You can use your physical Starbucks Card or your
+                          Starbucks India mobile app to purchase any item at all
+                          participating Starbucks stores in India. For example-
+                          food, beverage, retail coffees, Ready to Drink, Ready
+                          to Eat merchandise etc. The value in your Starbucks
+                          Card cannot be exchanged for cash.
+                        </p>
+                      </div>
                     </AccordionItemPanel>
                   </AccordionItem>
                 </Accordion>
@@ -59,19 +63,22 @@ const Faq = () => {
                   <AccordionItem>
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                      <span className="text-3xl font-semibol text-green-500">How to buy products at a time ?</span>
+                        <span className="text-xl font-semibol text-green-500">
+                          How to buy products at a time ?
+                        </span>
                       </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                       <div className="flex mt-8 ">
-                    <span className="border border-green-500"></span>
+                        <span className="border border-green-500"></span>
 
-                    <p className="max-w-3xl  text-gray-500 dark:text-gray-400 bg-green-100 p-10">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Magni, eum quae. Harum officiis reprehenderit ex quia
-                      ducimus minima id provident molestias optio nam vel..
-                    </p>
-                  </div>
+                        <p className="max-w-3xl  text-gray-500 dark:text-gray-400 bg-green-100 p-10">
+                          Physical Starbucks Cards are available in at all
+                          Starbucks stores in India or download Starbucks India
+                          mobile app and add a Starbucks e-card onto your
+                          registered Starbucks India account.
+                        </p>
+                      </div>
                     </AccordionItemPanel>
                   </AccordionItem>
                 </Accordion>
@@ -79,19 +86,22 @@ const Faq = () => {
                   <AccordionItem>
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                        <span className="text-3xl font-semibol text-green-500">Refund policy for customar ?</span>
+                        <span className="text-xl font-semibol text-green-500">
+                          Refund policy for customar ?
+                        </span>
                       </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                       <div className="flex mt-8">
-                    <span className="border border-green-500"></span>
+                        <span className="border border-green-500"></span>
 
-                    <p className="max-w-3xl px-4 text-gray-500 dark:text-gray-400 bg-green-100 p-10">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Magni, eum quae. Harum officiis reprehenderit ex quia
-                      ducimus minima id provident molestias optio nam vel..
-                    </p>
-                  </div>
+                        <p className="max-w-3xl px-4 text-gray-500 dark:text-gray-400 bg-green-100 p-10">
+                          Starbucks Cards are available in at all Starbucks
+                          stores in India or download Starbucks India mobile app
+                          and add a Starbucks e-card onto your registered
+                          Starbucks India account.
+                        </p>
+                      </div>
                     </AccordionItemPanel>
                   </AccordionItem>
                 </Accordion>
@@ -99,19 +109,23 @@ const Faq = () => {
                   <AccordionItem>
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                        <span className="text-3xl font-semibol text-green-500"> Exchange policy for a customar?</span>
+                        <span className="text-xl font-semibol text-green-500">
+                          {" "}
+                          Exchange policy for a customar?
+                        </span>
                       </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                       <div className="flex mt-8 md:mx-10">
-                    <span className="border border-green-500"></span>
+                        <span className="border border-green-500"></span>
 
-                    <p className="max-w-3xl px-4 text-gray-500 dark:text-gray-400 bg-green-100 p-10">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Magni, eum quae. Harum officiis reprehenderit ex quia
-                      ducimus minima id provident molestias optio nam vel..
-                    </p>
-                  </div>
+                        <p className="max-w-3xl px-4 text-gray-500 dark:text-gray-400 bg-green-100 p-10">
+                          Starbucks Cards are available in at all Starbucks
+                          stores in India or download Starbucks India mobile app
+                          and add a Starbucks e-card onto your registered
+                          Starbucks India account.
+                        </p>
+                      </div>
                     </AccordionItemPanel>
                   </AccordionItem>
                 </Accordion>
