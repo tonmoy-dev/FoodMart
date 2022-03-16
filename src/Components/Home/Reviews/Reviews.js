fdddd264faed9f31eb8c212298d555828bb5f337
showravdas11 <showravdas11@gmail.com>
2022-03-17 03:18:45 +0600

    review update

--- src/Components/Home/Reviews/Reviews.js
+++ src/Components/Home/Reviews/Reviews.js
@@ -33,78 +33,33 @@ const Reviews = ({reviews}) => {
           }}
           className={reviewStyle.swiper}
         >
-          <SwiperSlide className={reviewStyle.swiperslide}>
+          {
+            reviews.map((r) => (
+              <SwiperSlide key={r._id} className={reviewStyle.swiperslide}>
             <div className={reviewStyle.containers}>
               <div className={reviewStyle.profile}>
                 <div className={reviewStyle.imgBox}>
                   <Image
                     className={reviewStyle.avator}
-                    src="https://i.ibb.co/5BTm8d4/photo-profil-301783868.jpg"
+                    src={r.imageUrl}
                     alt=""
                     height={80}
                     width={80}
                   />
                 </div>
-                <h2>Person 1</h2>
+                <h2>{r.userName}</h2>
               </div>
               <div>
                 <p className={reviewStyle.reviewdetails}>
                   {" "}
-                  <b className={reviewStyle.qoute}>“</b> Thank you for letting me be a part of such a fantastic event! Being a volunteer for this race is much more than just
-             work, it&apos;s an unforgettable experience that will stay with me for the rest of my life.{" "}
+                  <b className={reviewStyle.qoute}>“</b> {r.description}
                   <b className={reviewStyle.qoute1}>“</b>
                 </p>
               </div>
             </div>
           </SwiperSlide>
-          <SwiperSlide className={reviewStyle.swiperslide}>
-            <div className={reviewStyle.containers}>
-              <div className={reviewStyle.profile}>
-                <div className={reviewStyle.imgBox}>
-                  <Image
-                    className={reviewStyle.avator}
-                    src="https://i.ibb.co/J2z1yM6/18dbd99c8f8166cb175c39506e15aa643856f4f0.jpg"
-                    alt=""
-                    height={80}
-                    width={80}
-                  />
-                </div>
-                <h2>Person 2</h2>
-              </div>
-              <div>
-                <p className={reviewStyle.reviewdetails}>
-                  {" "}
-                  <b className={reviewStyle.qoute}>“</b> Thank you for letting me be a part of such a fantastic event! Being a volunteer for this race is much more than just
-             work, it&apos;s an unforgettable experience that will stay with me for the rest of my life.{" "}
-                  <b className={reviewStyle.qoute1}>“</b>
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className={reviewStyle.swiperslide}>
-            <div className={reviewStyle.containers}>
-              <div className={reviewStyle.profile}>
-                <div className={reviewStyle.imgBox}>
-                  <Image
-                    className={reviewStyle.avator}
-                    src="https://i.ibb.co/xFfmcMN/Emotional-headshot-of-aspiring-actress-on-white-background-made-by-Headshots-Prague-1-1.jpg"
-                    alt=""
-                    height={80}
-                    width={80}
-                  />
-                </div>
-                <h2>Person 3</h2>
-              </div>
-              <div>
-                <p className={reviewStyle.reviewdetails}>
-                  {" "}
-                  <b className={reviewStyle.qoute}>“</b> Thank you for letting me be a part of such a fantastic event! Being a volunteer for this race is much more than just
-             work, it&apos;s an unforgettable experience that will stay with me for the rest of my life.{" "}
-                  <b className={reviewStyle.qoute1}>“</b>
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
+            ))
+          }
         </Swiper>
       </section>
     </>
