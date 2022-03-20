f0c40faafe0366e9d76530aef6625a6297a40f07
showravdas11 <showravdas11@gmail.com>
2022-03-21 01:39:56 +0600

    chat

---
+++ pages/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TawkMessengerReact from '/node_modules/@tawk.to/tawk-messenger-react';
+
+const test = () => {
+    return (
+        <div>
+            <h2>Test</h2>
+            <TawkMessengerReact
+                propertyId="5f8874962901b9207693c6bb"
+                widgetId="5f8874962901b9207693c6bb"/>
+        </div>
+    );
+};
+
+export default test;
