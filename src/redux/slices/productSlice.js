f1943f4807d3b2a5e0825b09649c233b86f957ee
Tonmoy Roy <rxdevs100@gmail.com>
2022-03-24 20:47:26 +0600

    localhost removed

--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -3,21 +3,21 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 export const fetchCartProducts = createAsyncThunk(
     'product/fetchCartProducts',
     async (user) => {
-        const response = await fetch(`http://localhost:3000/api/cart?email=${user.email}`)
+        const response = await fetch(`https://food-mart-web.vercel.app/api/cart?email=${user.email}`)
             .then(res => res.json())
         return response
     });
 export const fetchCompareProducts = createAsyncThunk(
     'product/fetchCompareProducts',
     async (user) => {
-        const response = await fetch(`http://localhost:3000/api/compare?email=${user.email}`)
+        const response = await fetch(`https://food-mart-web.vercel.app/api/compare?email=${user.email}`)
             .then(res => res.json())
         return response
     });
 export const fetchWishlistProducts = createAsyncThunk(
     'product/fetchWishlistProducts',
     async (user) => {
-        const response = await fetch(`http://localhost:3000/api/wishlists?email=${user.email}`)
+        const response = await fetch(`https://food-mart-web.vercel.app/api/wishlists?email=${user.email}`)
             .then(res => res.json())
         return response
     });
