8663dbfa6239a1ef07469be6a530899a04283857
apudebnath <apu1578@gmail.com>
2022-03-27 01:54:35 +0600

    compare page updated

--- pages/compare/index.js
+++ pages/compare/index.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import Image from "next/image";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import { fetchCartProducts, fetchCompareProducts } from "../../src/redux/slices/productSlice";
+import "react-toastify/dist/ReactToastify.css";
+import {
+    fetchCartProducts,
+    fetchCompareProducts,
+} from "../../src/redux/slices/productSlice";
 
 const Compare = () => {
     const [control, setControl] = useState(false);
@@ -17,7 +20,7 @@ const Compare = () => {
     // private routing
     const router = useRouter();
     if (!user?.email) {
-        router.push('/login');
+        router.push("/login");
     }
 
     useEffect(() => {
@@ -38,15 +41,15 @@ const Compare = () => {
                 price: price,
                 description: title,
                 quantity: 1,
-                email:user.email
+                email: user.email,
             })
             .then((response) => {
                 if (response.data.insertedId) {
                     setControl(!control);
                     dispatch(fetchCartProducts(user));
-                    toast.success('Wow! Added to your cart.', {
-                        position: "bottom-left"
-                      });
+                    toast.success("Wow! Added to your cart.", {
+                        position: "bottom-left",
+                    });
                 } else {
                     setControl(false);
                 }
@@ -58,9 +61,9 @@ const Compare = () => {
             if (response.data.deletedCount) {
                 setControl(!control);
                 dispatch(fetchCompareProducts(user));
-                toast.warn('Removed a product.', {
-                    position: "bottom-left"
-                  });
+                toast.warn("Removed a product.", {
+                    position: "bottom-left",
+                });
             } else {
                 setControl(false);
             }
@@ -69,24 +72,23 @@ const Compare = () => {
 
     return (
         <div div className="py-16 mx-5 md:mx-20">
-            <h2 className=" text-black font-semibold text-3xl text-center pb-2">
+            <h2 className=" text-black font-semibold text-2xl md:text-3xl text-center pb-2">
                 Products Compare
             </h2>
 
-            <div className="">
+            <div className="w-full">
                 {/* wishlist table */}
                 <div className="pt-10 overflow-auto">
                     <table className="w-full">
-                        
-                        <tbody className="flex border rounded p-4 overflow-auto w-full">
+                        <tbody className="flex justify-center rounded py-2 overflow-auto w-full">
                             {products.length !== 0 && (
-                                <tr className="flex flex-col w-1/4  border">
-                                    <td className="p-3 h-32 md:h-48 border-b">
+                                <tr className="flex flex-col w-28 md:w-36  border">
+                                    <td className="p-3 h-28 md:h-40 border-b">
                                         <p className="text-sm font-semibold text-gray-500">
                                             Preview
                                         </p>
                                     </td>
-                                    <td className="p-4 border-b">
+                                    <td className="h-24 md:h-12 p-4 border-b">
                                         <p className="text-sm font-semibold text-gray-500">
                                             Name
                                         </p>
@@ -101,7 +103,7 @@ const Compare = () => {
                                             Rating
                                         </h3>
                                     </td>
-                                    <td className="h-40 p-4 border-b">
+                                    <td className="h-36 p-4 border-b">
                                         <h3 className="text-sm font-semibold text-gray-500">
                                             Description
                                         </h3>
@@ -111,20 +113,20 @@ const Compare = () => {
                                             Stock status
                                         </h3>
                                     </td>
-                                    <td className="p-4 border-b">
+                                    <td className="h-16 p-4 border-b">
                                         <h3 className="text-sm font-semibold text-gray-500">
                                             Buy now
                                         </h3>
                                     </td>
-                                    <td className="p-4 border-b">
+                                    <td className="p-4">
                                         <h3 className="text-sm font-semibold text-gray-500">
-                                            Delete Compare Product
+                                            Action
                                         </h3>
                                     </td>
                                 </tr>
                             )}
                             {products == false && (
-                                <h2 className="text-2xl text-center m-auto py-12">
+                                <h2 className="text-2xl text-gray-400 text-center m-auto py-12">
                                     You have no Compare Products! Please add
                                     Products.
                                 </h2>
@@ -143,25 +145,25 @@ const Compare = () => {
                                 return (
                                     <tr
                                         key={_id}
-                                        className="flex flex-col w-1/4  border"
+                                        className="flex flex-col w-36 md:w-1/4  border text-center"
                                     >
-                                        <td className="flex justify-center items-center h-32 md:h-48 border-b">
+                                        <td className="flex justify-center items-center h-28 md:h-40 border-b">
                                             <Image
                                                 src={product_imageUrl}
                                                 alt="product image"
                                                 className="object-cover"
-                                                width="175px"
-                                                height="140px"
+                                                width="105px"
+                                                height="100px"
                                             />
                                         </td>
-                                        <td className="p-4 border-b">
+                                        <td className="h-24 md:h-12 p-4 border-b">
                                             <p className="text-sm font-semibold text-gray-500">
-                                                {product_title}
+                                                {product_title.slice(0,30)}
                                             </p>
                                         </td>
                                         <td className="p-4 border-b">
                                             <p className="text-sm font-semibold text-gray-500">
-                                                {product_price}
+                                                $ {product_price}
                                             </p>
                                         </td>
                                         <td className="p-4 border-b">
@@ -169,17 +171,17 @@ const Compare = () => {
                                                 {user_rating}
                                             </h3>
                                         </td>
-                                        {/* <td className="h-40 p-4 border-b overflow-scroll">
-                                            <h3 className="text-sm font-semibold text-gray-500">
+                                        <td className="h-36 p-4 border-b overflow-scroll">
+                                            <h3 className="text-xs md:sm font-semibold text-gray-500">
                                                 {produc_Details.slice(0, 180)}
                                             </h3>
-                                        </td> */}
+                                        </td>
                                         <td className="p-4 border-b">
                                             <span className="p-1.5 text-xs font-medium uppercase tracking-wider primary-color bg-green-200 rounded-md bg-opacity-50">
                                                 {product_stock}
                                             </span>
                                         </td>
-                                        <td className="p-4 border-b">
+                                        <td className="h-16 p-4 border-b">
                                             <button
                                                 onClick={() =>
                                                     addToCartHandler(
@@ -188,7 +190,7 @@ const Compare = () => {
                                                         product_price
                                                     )
                                                 }
-                                                className="p-2 text-xs font-medium uppercase tracking-wider primary-color bg-green-300 rounded-md bg-opacity-50 border border-green-300 hover:bg-opacity-80 hover:text-green-600"
+                                                className="p-2 text-xs font-semibold uppercase tracking-wider primary-color bg-green-300 rounded-md bg-opacity-50 border border-green-300 hover:bg-opacity-80 hover:text-green-600"
                                             >
                                                 Add to cart
                                             </button>
